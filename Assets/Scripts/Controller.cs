31b0f67595150bab3123a3c0d20610318939589a
Luis Jose Medina Sandoval <luis06jose@gmail.com>
2019-10-09 05:15:30 -0400

    Se le agrego y corrigio movimiento al personaje principal

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControladorMovimiento : MonoBehaviour
+public class Controller : MonoBehaviour
 {
+    public GameObject pupusito;
+
     // Start is called before the first frame update
-    void Start()
+    void Start ()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    void Update ()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+        {
+            Move.Instance.ToLeft(pupusito);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Move.Instance.ToRight(pupusito);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A) ||  Input.GetKeyUp(KeyCode.D))
+        {
+            Move.Instance.Nothing(pupusito);
+        }
     }
 }
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move
+{
+    public static Move Instance = new Move();
+
+    public void ToRight(GameObject gameObject)
+    {
+        if (gameObject.GetComponent<SpriteRenderer>().flipX)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        gameObject.GetComponent<Animator>().SetBool("To Walk", true);
+        gameObject.GetComponent<Transform>().Translate(Vector3.right * Time.deltaTime * 5);
+    }
+
+    public void ToLeft(GameObject gameObject)
+    {
+        if (!gameObject.GetComponent<SpriteRenderer>().flipX)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        gameObject.GetComponent<Animator>().SetBool("To Walk", true);
+        gameObject.GetComponent<Transform>().Translate(Vector3.left * Time.deltaTime * 5);
+    }
+
+    public void Nothing(GameObject gameObject)
+    {
+        gameObject.GetComponent<Animator>().SetBool("To Walk", false);
+    }
+
+    private Move() { }
+}
