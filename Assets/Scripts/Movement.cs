a8a7ed7656ffcb53d9ff91d72b5cc31ca108abdd
Samuel Contreras Machado <elsams321@outlook.es>
2019-10-09 04:08:43 -0400

    Inicio del prooyecto con movimiento del personaje en horizontal, y sus animaciones

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class NewBehaviourScript
+{
+    public static void Derecha(GameObject gameObject)
+    {
+        if (Input.GetKey("D"))
+        {
+            if (gameObject.GetComponent<SpriteRenderer>().flipX)
+            {
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            }
+            gameObject.GetComponent<Animator>().SetBool("To Walk", true);
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.2f, 0));
+        }
+
+        if (Input.GetKey("A"))
+        {
+            if (!gameObject.GetComponent<SpriteRenderer>().flipX)
+            {
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
+            gameObject.GetComponent<Animator>().SetBool("To Walk", true);
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-0.2f, 0));
+        }
+
+        if(Input.GetKeyUp("A") || Input.GetKeyUp("D"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("To Walk", false);
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Pupusito-s-Adventure
+This game will be the best game that play store never have. Write it.
