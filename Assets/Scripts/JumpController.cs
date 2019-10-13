e76ce26643688bac68dcd0f0b943b829c6d0119e
Luis Jose Medina Sandoval <luis06jose@gmail.com>
2019-10-13 17:22:31 -0400

    Se le agrego la accion de saltar al personaje

---
+++ Assets/Scripts/JumpController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpController : MonoBehaviour
+{
+    public GameObject Player;
+    public Transform FloorChecker;
+    public LayerMask FloorLayer;
+    private bool ContactFloor = true;
+    private readonly float jumpForce = 800;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        UpdateContactFloor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && ContactFloor)
+        {
+            Player.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce);
+        }
+    }
+
+    void UpdateContactFloor()
+    {
+        ContactFloor = Physics2D.OverlapCircle(FloorChecker.position, 0.07f, FloorLayer);
+    }
+}
