20e4e9bcc2224c93913f52d10340ca343b6ca5d3
Samuel Contreras Machado <elsams321@outlook.es>
2019-11-01 00:26:10 -0400

    Cambios de código
    Se refactorizó todo el código, y se dejó más ordenado.

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Character
+{
+    public int Lives { private get; set; }
+    private GameObject Instance { get; set; }
+    private Transform FloorChecker { get; set; }
+    public bool ContactFloor { get; private set; }
+
+    public void MoveRight()
+    {
+        if (Instance.GetComponent<SpriteRenderer>().flipX)
+        {
+            Instance.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        Instance.GetComponent<Animator>().SetBool("To Walk", true);
+        Instance.GetComponent<Transform>().Translate(Vector3.right * Time.deltaTime * 10);
+    }
+
+    public void MoveLeft()
+    {
+        if (!Instance.GetComponent<SpriteRenderer>().flipX)
+        {
+            Instance.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        Instance.GetComponent<Animator>().SetBool("To Walk", true);
+        Instance.GetComponent<Transform>().Translate(Vector3.left * Time.deltaTime * 10);
+    }
+
+    public void Still()
+    {
+        Instance.GetComponent<Animator>().SetBool("To Walk", false);
+    }
+
+    public void Jump()
+    {
+        Instance.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1000);
+    }
+
+    public IEnumerator CheckContactFloor(int FloorLayer)
+    {
+        WaitForFixedUpdate waitForFixedUpdate = new WaitForFixedUpdate();
+        while (true)
+        {
+            ContactFloor = Physics2D.OverlapCircle(FloorChecker.position, 0.07f, FloorLayer);
+            yield return waitForFixedUpdate;
+        }
+    }
+
+    public void SetJumpingAnimation()
+    {
+        Instance.GetComponent<Animator>().SetBool("Jumping", !ContactFloor);
+    }
+
+    public Character(GameObject Instance)
+    {
+        this.Instance = Instance;
+        Lives = 2;
+
+        // Busca el Floor Checker hijo y lo asigna a la variable FloorChecker
+        for (int i = 0; i < Instance.GetComponent<Transform>().childCount; i++)
+        {
+            if (Instance.GetComponent<Transform>().GetChild(i).name == "Floor Checker")
+                FloorChecker = Instance.GetComponent<Transform>().GetChild(i);
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    Character PupusitoGreen;
+
+    public GameObject PupusitoGreenInstance;
+
+    public LayerMask FloorLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PupusitoGreen = new Character(PupusitoGreenInstance);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            PupusitoGreen.MoveLeft();
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            PupusitoGreen.MoveRight();
+        }
+
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
+        {
+            PupusitoGreen.Still();
+        }
+
+        if (Input.GetKey(KeyCode.W) && PupusitoGreen.ContactFloor)
+        {
+            PupusitoGreen.Jump();
+        }
+        StartCoroutine(PupusitoGreen.CheckContactFloor(FloorLayer));
+        PupusitoGreen.SetJumpingAnimation();
+    }
+}
